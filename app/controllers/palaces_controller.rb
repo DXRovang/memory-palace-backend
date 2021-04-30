7b2deaab05dcf9d27b3f768c4e6555c2465cd444
Alexander Rovang <wolf_vii@yahoo.com>
2021-04-29 22:38:14 -0400

    checks to see if render works

--- app/controllers/palaces_controller.rb
+++ app/controllers/palaces_controller.rb
@@ -1,2 +1,11 @@
 class PalacesController < ApplicationController
+
+  def index
+    # read up on why/how the render() method works with a hash, 
+    # particularly in it's treatement of json as a key
+    # this is a replacement for .to_json
+    render(json: Palace.all)
+  end
+
+
 end
